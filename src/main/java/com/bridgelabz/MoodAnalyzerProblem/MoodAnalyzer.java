856e2473e9730d2074c2e369e5462782bcc9f711
sahana199835 <tnsahana1@gmail.com>
2023-04-15 20:53:31 +0530

    add:check invalid enum

--- src/main/java/com/bridgelabz/MoodAnalyzerProblem/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/MoodAnalyzerProblem/MoodAnalyzer.java
@@ -2,36 +2,76 @@ package com.bridgelabz.MoodAnalyzerProblem;
 
 public class MoodAnalyzer {
 	
-	private String message; // Field to store the mood message
-	// Default constructor
-	public MoodAnalyser() {
-		message = ""; // Set default empty message
-	}
-	// Constructor with message parameter
-	public MoodAnalyser(String message) {
-		this.message = message; // Set the message field with the passed parameter
-	}
+	import java.util.HashMap;
+	import java.util.Map;
 
-	// Analysis mood method using the message field
-	public String analysisMood() {
-		if (message.contains("Sad")) {
-			return "SAD";
-		} else {
-		try {
-			if (message == null || message.isEmpty()) {
-				throw new InvalidMoodException("Invalid mood: Mood message cannot be null or empty.");
-			} else if (message.contains("Sad")) {
-				return "SAD";
-			} else {
+		private String message; // Field to store the mood message
+	enum MoodEnum {
+		HAPPY("happy"), SAD("sad"), ANGRY("angry"), NEUTRAL("neutral");
+
+		// Default constructor
+		public MoodAnalyser() {
+			message = ""; // Set default empty message
+		private String value;
+		private static Map<String, MoodEnum> map = new HashMap<>();
+
+		static {
+			for (MoodEnum mood : MoodEnum.values()) {
+				map.put(mood.value, mood);
+			}
+		}
+
+		// Constructor with message parameter
+		public MoodAnalyser(String message) {
+			this.message = message; // Set the message field with the passed parameter
+		MoodEnum(String value) {
+			this.value = value;
+		}
+
+		// Analysis mood method using the message field
+		public String analysisMood() {
+			try {
+				if (message == null || message.isEmpty()) {
+					throw new InvalidMoodException("Invalid mood: Mood message cannot be null or empty.");
+				} else if (message.contains("Sad")) {
+					return "SAD";
+				} else {
+					return "HAPPY";
+				}
+			} catch (InvalidMoodException e) {
+				// Handle the exception and return "HAPPY" as default value
+				System.out.println(e.getMessage());
 				return "HAPPY";
 			}
-		} catch (InvalidMoodException e) {
-			// Handle the exception and return "HAPPY" as default value
-			System.out.println(e.getMessage());
-			return "HAPPY";
+		public String getValue() {
+			return value;
+		}
+
+		public static MoodEnum fromValue(String value) {
+			return map.get(value.toLowerCase());
 		}
 	}
+
+	// Define MoodAnalyzer class
+	class MoodAnalyzer {
+		public String analyzeMood(String mood) throws MoodAnalysisException {
+			// Check if mood is null or empty
+			if (mood == null || mood.trim().isEmpty()) {
+				throw new MoodAnalysisException("Invalid Mood: Mood cannot be empty or null.");
+			}
+
+			// Convert mood to lowercase for case-insensitive comparison
+			mood = mood.toLowerCase();
+
+			// Check if mood is valid using Enum
+			MoodEnum moodEnum = MoodEnum.fromValue(mood);
+			if (moodEnum == null) {
+				throw new MoodAnalysisException("Invalid Mood: Mood not recognized.");
+			}
+
+			// Return the analyzed mood
+			return mood;
+		}
 	}
-}
-
-
+	 
+	
--- src/test/java/com/bridgelabz/MoodAnalyzerProblem/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyzerProblem/MoodAnalyzerTest.java
@@ -6,13 +6,23 @@ import org.junit.Test;
 
 public class MoodAnalyzerTest {
 	
+	public static void main(String[] args) {
+        try {
+            // Test case 1: Null mood
+            String mood1 = null;
+            MoodAnalyzer moodAnalyzer1 = new MoodAnalyzer();
+            String analyzedMood1 = moodAnalyzer1.analyzeMood(mood1);
+            System.out.println("Analyzed Mood 1: " + analyzedMood1);
+        } catch (MoodAnalysisException e) {
+            System.out.println("Error 1: " + e.getMessage());
+        }
+
 	@Test
 	public void testAnalysisMoodSad() {
 		MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood");
 		String mood = moodAnalyser.analysisMood();
 		assertEquals("SAD", mood);
 	}
-}
 
 	@Test
 	public void testAnalysisMoodHappy() {
@@ -27,7 +37,26 @@ public class MoodAnalyzerTest {
 		String mood = moodAnalyser.analysisMood();
 		assertEquals("HAPPY", mood);
 	}
+        try {
+            // Test case 2: Empty mood
+            String mood2 = "";
+            MoodAnalyzer moodAnalyzer2 = new MoodAnalyzer();
+            String analyzedMood2 = moodAnalyzer2.analyzeMood(mood2);
+            System.out.println("Analyzed Mood 2: " + analyzedMood2);
+        } catch (MoodAnalysisException e) {
+            System.out.println("Error 2: " + e.getMessage());
+        }
 
+        try {
+            // Test case 3: Valid mood
+            String mood3 = "happy";
+            MoodAnalyzer moodAnalyzer3 = new MoodAnalyzer();
+            String analyzedMood3 = moodAnalyzer3.analyzeMood(mood3);
+            System.out.println("Analyzed Mood 3: " + analyzedMood3);
+        } catch (MoodAnalysisException e) {
+            System.out.println("Error 3: " + e.getMessage());
+        }
+    }
 }
 
 
